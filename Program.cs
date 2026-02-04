83c80832132637c407c03e80b1a4dd8b7a8f97ad
Mark Griep <123456+markgriep@users.noreply.github.com>
2026-02-03 20:31:27 -0600

    removed extranous code.

--- Program.cs
+++ Program.cs
@@ -56,8 +56,7 @@ builder.Services.AddSingleton(new OpenAiSettings
     Temperature = openAiTemperature
 });
 
-//var openAiApiKey = builder.Configuration["OpenAI:ApiKey"]
-//    ?? throw new InvalidOperationException("Missing OpenAI:ApiKey secret.");
+
 
 builder.Services.AddSingleton(new OpenAiSettings { ApiKey = openAiApiKey });
 
